c64341b0c98a7e7c9b1f12157fe53332d2e85e02
BANANASJIM <bananasjim1@gmail.com>
2025-09-17 23:42:38 -0700

    add gltf style scene system

--- fix_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Fix designated initializers issue - vulkan-hpp doesn't support them with older compilers
-# Fix EASTL container compatibility issues
-
-echo "Applying build fixes..."
-
-# Need to rewrite the code to be compatible with older C++
